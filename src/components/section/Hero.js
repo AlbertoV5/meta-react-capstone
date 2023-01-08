41c0835d18bc407afa5ddc058a0697c9100fccfc
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-01-08 13:01:08 -0600

    fixing components for sections

--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -5,16 +5,18 @@ const Hero = () => {
         <Wrap 
             as="section" 
             id="hero-section"
-            spacing={"10"}
+            spacing={"5"}
             width={{
                 base:"70vw",
                 md:"70vw"
             }}
+            align={"center"}
+            py={"5"}
         >
             <VStack
                 id="hero-header"
-                spacing={"10"}
-                align='stretch'
+                spacing={"4"}
+                align={"stretch"}
                 width={{
                     base:"100%",
                     md:"40%"
@@ -22,22 +24,23 @@ const Hero = () => {
             >
                 <Heading textAlign={"left"}>Little Lemon</Heading>
                 <Heading textAlign={"left"} size="sm">Chicago</Heading>
-                <Text textAlign={"justify"}
+                <Text textAlign={"justify"} py={"4"}
                 >Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
                 nisi ut aliquip ex ea commodo consequat.
                 </Text>
+                <Spacer></Spacer>
                 <Button width={"50%"}
                     className="btn btn-primary"
                 >Press Me!
                 </Button>
             </VStack>
-            {/* <Spacer height={"0"}></Spacer> */}
-            <VStack
+            <Spacer height={"0"}></Spacer>
+            <HStack
                 width={{
                     base:"100%",
-                    md:"40%"
+                    md:"45%"
                 }}
             >
                 <Image
@@ -45,7 +48,7 @@ const Hero = () => {
                     alt="picture of dessert"
                 >
                 </Image>
-            </VStack>
+            </HStack>
     </Wrap>
     )
 }
