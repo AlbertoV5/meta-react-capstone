9b3abfbf5fabef51e6d4ed5bc9b2dacbef4a1694
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-01-09 18:52:01 -0600

    completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,77 @@
 import MenuLinks from "./links/MenuLinks"
 
+import SectionBase from "./section/SectionBase";
+
+import {Link} from "react-router-dom"
+
+import { HStack, VStack, Image, Spacer, Box, Heading, Text, UnorderedList, ListItem, Wrap } from "@chakra-ui/react";
+
 const Footer = () => {
    return (
-    <footer>
-      <img 
-      src="https://www.kyleecooks.com/wp-content/uploads/2019/03/Mini-Lemon-Meringue-Pies-28.jpg"
-      ></img>
-      <div>
-         <h4>Doormat Navigation</h4>
-         <MenuLinks/>
-      </div>
-      <div>
-         <h4>Social Media Links</h4>
-         <ul>
-            <li>Twitter</li>
-            <li>Instagram</li>
-            <li>YouTube</li>
-         </ul>
-      </div>
-    </footer>
+    <SectionBase 
+      as="footer"
+      backgroundColor={"gold"}
+   >
+      <Wrap
+         id="footer-wrapper"
+         spacing={"4"}
+         align={"stretch"}
+         width={{
+               base:"100%",
+               md:"100%"
+         }}
+         paddingTop={"1.5em"}
+         spacingY={"8"}
+      >
+         <Image
+            width={"150px"}
+            src="https://www.kyleecooks.com/wp-content/uploads/2019/03/Mini-Lemon-Meringue-Pies-28.jpg"
+         ></Image>
+         <Spacer></Spacer>
+         <Box width={{
+            base:"40%",
+            md:"20%"
+         }}>
+            <Heading size="sm">Doormat Navigation</Heading>
+            <br></br>
+            <VStack align={"left"}>
+               <Link>Home</Link>
+               <Link>About</Link>
+               <Link>Menu</Link>
+               <Link>Reservations</Link>
+               <Link>Order Online</Link>
+               <Link>Login</Link>
+            </VStack>       
+         </Box>
+         <Spacer></Spacer>
+         <Box width={{
+            base:"40%",
+            md:"20%"
+         }}>
+            <Heading size="sm">Contact</Heading>
+            <br></br>
+            <VStack align={"left"}>
+               <Link>Address</Link>
+               <Link>Phone Number</Link>
+               <Link>Email</Link>
+            </VStack>
+         </Box>
+         <Spacer></Spacer>
+         <Box width={{
+            base:"40%",
+            md:"20%"
+         }}>
+            <Heading size="sm">Social Media Links</Heading>
+            <br></br>
+            <VStack align={"left"}>
+               <Link>Twitter</Link>
+               <Link>Instagram</Link>
+               <Link>YouTube</Link>
+            </VStack>
+         </Box>
+         <Spacer></Spacer>
+      </Wrap>
+    </SectionBase>
    ) 
 }
 export default Footer;
